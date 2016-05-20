4e25afa2508db5b8923b0ef982324321514a39ad
root <jainamritanshu@gmail.com>
2016-05-20 14:26:23 +0530

    working on models

---
+++ main/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class customer(models.Model):
+	prod = models.CharField(max_length=300, is_required = True)
+	Cons_name = models.CharField(max_length = 50, is_required = True)
+	gender = models.ChoiceField(choices = Gender, is_required = True)
+	Age = models.IntegerField(max_length = 2, is_required = True)
+	add = models.CharField(max_length = 500, is_required = True)
+	prov = models.CharField(max_length=30, is_required = True)
+	zip_c = models.IntegerField(max_length=10, is_required = True)
+	country = models.CharField(max_length=20, is_required = True)
+	phone_no = models.IntegerField(max_length=15, is_required = True)
+	Ext = models.IntegerField(max_length=4, is_required = True)
+	receipt = models.FileField(upload_to=#dekhte hain, #to be completed)
+
+class ticket(models.Model):
+	choice_ticket = {query = 'query',
+			  order = 'order',
+			  }
+	choice_temp = {
+					#make a dict of templates used by agents
+	}
+	ticket = models.ChoiceField(choices = choice_ticket, is_required = True)
+class agent(models.Model):
+	#some shitty details
+
+class message(models.Model):
+	msg_text = models.CharField(max_length = 1000, is_required = True)
+
+class reply(models.Model):
+	rep_text = models.ForeignKey(message, max_length = 1000, is_required = True)
+	templates = models.ChoiceField(choices = choice_temp)
